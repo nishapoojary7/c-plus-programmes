4b98ea070480a092be09168f7dbe13948ddf78c7
Nisha <117558347+nishapoojary7@users.noreply.github.com>
2023-01-09 21:16:43 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    int a,b,temp;
+    cout<<"enter the value of a"<<endl;
+    cin>>a;
+    cout<"enter the value of b"<<endl;
+    cin>>b;
+    cout<"the values before swapping"<<endl;
+    cout<<"the values of a="<<a<<endl;
+    cout<<"the values of b="<<b<<endl;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"values of the swapping"<<endl;
+    cout<<"values of a="<<a<<endl;
+      cout<<"values of b="<<b<<endl;
+}
+    
+
+}
